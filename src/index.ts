efeee74c99725d9a0fbde552557e4b70d56236a5
Jeswin Kumar <jeswinpk@agilehead.com>
2018-10-06 18:40:20 +0530

    Figuring out the client API.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,58 @@
-const sync = require("libpeer");
-const store = require("store");
+/* This is the module for the p2p bot */
+const libpeer = require("libpeer");
 
+/* Some APP Specific Details */
 const APP_ID = "BLABBEROMA";
+const appDataStore = new Store();
 
 async function init() {
-  await sync.ensureStreams(["statusfeed"]);
+  // Create a client. Param is in multiaddr format.
+  const client = new libpeer.Client("/ip4/1.2.3.4/tcp/443/");
 
-  const peers = await sync.getPeers(APP_ID);
-  
+  // Make sure we have a stream to write into.
+  // This is to write the users status messages and images
+  await libpeer.ensureStreams([
+    { name: "statusfeed", concatMessages: true, rotation: "size", maxKB: 1000 },
+    { name: "images", concatMessages: false, encrypt: false }
+  ]);
+
+  // Get a list of peers
+  const peers = await libpeer.getPeers(APP_ID);
+
   for (const peer of peers) {
-    const { data, blobs } = await store.getPeerState(peer.id);
-    peer.readStream(data.statusfeed.lastSynced, async (msg: string) => {
-      processData(msg);
+    // For a peer, get the last-synced msgid for statusfeed and images.
+    const {
+      streams: { statusfeed, images }
+    } = await appDataStore.getPeerState(peer.id);
+
+    // Read the peer's statusfeed
+    peer.readStream(statusfeed.lastSynced, async (msg: string) => {
+      writeToLocalDB(msg);
     });
-    peer.readBlobStream(blobs.statusfeed.lastSynced, async (msg: string) => {
-      processData(msg);
+
+    // Read the peer's blob stream
+    peer.readStream(images.lastSynced, async (msg: string) => {
+      writeImage(msg);
     });
   }
 }
 
-async function processData(data: string) {
+async function writeToLocalDB(data: string) {
+  // Write to an sqlite database or something.
+  // Table schemas are app specific.
   return;
 }
 
+async function writeImage(data: string) {
+  // Write to a jpg or png file.
+  return;
+}
+
+/*
+  Show a UI.
+  Read from sqlite.
+  Display some images etc.
+*/
+async function startUI() {
+  return "<div>Hello World....(content goes here)</div>";
+}
